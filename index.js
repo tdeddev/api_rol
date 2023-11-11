140d75ad39a72fd45cee6d095a3275b5b189f2c5
tdedwin <nuttawut.c39@gmail.com>
2023-11-11 13:16:06 +0700

    update cors

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = 4000;
 const BASE_URL = 'https://apps.maxion.gg/api/market/list?status=LISTING&category=headgear&serverId=1'
 const buy_url = 'https://apps.maxion.gg/roverse/detail/'
 const BASE_URL_Img = 'https://apps.maxion.gg/_next/image?url=https%3A%2F%2Frop2e-collection-cdn.s3-bkk.nipa.cloud%2F'
+const cors = require('cors')
 
 app.listen(port, () => console.log(`App Listening on ${port}`))
+
+app.use(cors({
+    credentials : true
+}))
 
 app.get('/', (req, res) => {
     res.send('This is my API running...')
